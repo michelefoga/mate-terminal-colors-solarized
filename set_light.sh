700742f12cca0e5e9652a415e3ced6b3825b0b2c
michelefoga <michelefoga@gmail.com>
2012-09-18 17:32:10 +0200

    Updated gnome version solarized theme for mate

--- set_light.sh
+++ set_light.sh
@@ -5,16 +5,16 @@ dir=`dirname $0`
 PROFILE=${1:-Default}
 
 # set palette
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/$PROFILE/palette `cat $dir/colors/palette`
+mateconftool-2 -s -t string /apps/mate-terminal/profiles/$PROFILE/palette `cat $dir/colors/palette`
 
 # set highlighted color to be different from foreground-color
-gconftool-2 -s -t bool /apps/gnome-terminal/profiles/$PROFILE/bold_color_same_as_fg false
+mateconftool-2 -s -t bool /apps/mate-terminal/profiles/$PROFILE/bold_color_same_as_fg false
 
 # set foreground to base00 and background to base3 and highlight color to
 # base01
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/$PROFILE/background_color `cat $dir/colors/base3`
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/$PROFILE/foreground_color `cat $dir/colors/base00`
-gconftool-2 -s -t string /apps/gnome-terminal/profiles/$PROFILE/bold_color `cat $dir/colors/base01`
+mateconftool-2 -s -t string /apps/mate-terminal/profiles/$PROFILE/background_color `cat $dir/colors/base3`
+mateconftool-2 -s -t string /apps/mate-terminal/profiles/$PROFILE/foreground_color `cat $dir/colors/base00`
+mateconftool-2 -s -t string /apps/mate-terminal/profiles/$PROFILE/bold_color `cat $dir/colors/base01`
 
 # make sure the profile is set to not use theme colors
-gconftool-2 -s -t bool /apps/gnome-terminal/profiles/$PROFILE/use_theme_colors false
+mateconftool-2 -s -t bool /apps/mate-terminal/profiles/$PROFILE/use_theme_colors false
